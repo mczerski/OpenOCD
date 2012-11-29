1048c5e014009a92f9f271279c48d8e3e478e132
Franck Jullien <franck.jullien@gmail.com>
2012-11-29 21:33:17 +0100

    helper/binarybuffer: introduce buffer shift right
    Introduce a buffer shift right function. This function
    is used in or1k_jtag_adv.c but could also be usefull
    somewhere else.
    
    Signed-off-by: Franck Jullien <franck.jullien@gmail.com>

--- src/helper/binarybuffer.h
+++ src/helper/binarybuffer.h
@@ -155,5 +155,6 @@ int bit_copy_queued(struct bit_copy_queue *q, uint8_t *dst, unsigned dst_offset,
 		    unsigned src_offset, unsigned bit_count);
 void bit_copy_execute(struct bit_copy_queue *q);
 void bit_copy_discard(struct bit_copy_queue *q);
+void buffer_shr(void *_buf, unsigned buf_len, unsigned count);
 
 #endif /* BINARYBUFFER_H */
